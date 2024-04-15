314387b865a6af5c823184f1a6afb27609fc451a
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2024-04-15 08:13:28 +0100

    Add certifications module

---
+++ server/src/main/java/project/server/certificat/CertificatRepo.java
@@ -0,0 +1,9 @@
+package project.server.certificat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CertificatRepo extends JpaRepository<Certificat,String> {
+    
+}
