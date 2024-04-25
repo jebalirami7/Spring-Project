1dd3c84a00b6d255040408ccfe53da91c7fdda66
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2024-04-25 02:27:07 +0100

    Add Course Material Entity, Dto and Mapper classes

--- server/src/main/java/project/server/Entities/CourseMaterial.java
+++ server/src/main/java/project/server/Entities/CourseMaterial.java
@@ -19,8 +19,9 @@ public class CourseMaterial {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    private String question;
-    private String[] reponses;
+    private String title;
+    private String content;
+    private String type;
 
     @ManyToOne
     @JoinColumn(name = "chapter_id")
