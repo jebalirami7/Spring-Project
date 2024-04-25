27b342371ace0fbd4c1b0cf1af302ba29cfc9f8d
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2024-04-25 14:26:11 +0100

    Add refresh-token endpoint to UserController

--- server/src/main/java/project/server/Controllers/UserController.java
+++ server/src/main/java/project/server/Controllers/UserController.java
@@ -9,11 +9,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import io.jsonwebtoken.ExpiredJwtException;
 
 @RestController
 @RequestMapping("/user")
@@ -55,6 +60,36 @@ public class UserController {
         } else {
             throw new UsernameNotFoundException("Invalid user request!");
         }
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<?> refreshToken(@RequestHeader("Authorization") String authorizationHeader) {
+        String username = null;
+        String refreshToken = null;
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            refreshToken = authorizationHeader.substring(7);
+            try {
+                username = jwtService.extractUsername(refreshToken);
+            } catch (ExpiredJwtException e) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh token expired");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid refresh token");
+            }
+        }
+        
+        if (username != null && refreshToken != null ) {
+            UserDetails userDetails = service.loadUserByUsername(username);
+            
+            if (jwtService.validateToken(refreshToken, userDetails)) {
+                String newAccessToken = jwtService.generateToken(userDetails.getUsername());
+                Map<String, String> response = new HashMap<>();
+                response.put("token", newAccessToken);
+                
+                return ResponseEntity.ok(response);
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
     }
 
 }
