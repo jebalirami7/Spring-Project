08193c7347b6d940bff324ecb314bb679bbe39d2
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2024-04-25 13:51:49 +0100

    Add image support to lecture material component

--- client/src/app/lecture-material/lecture-material.component.ts
+++ client/src/app/lecture-material/lecture-material.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LectureService } from '../services/lecture.service';
 import { LectureDTO } from '../models/DTO/LectureDTO';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-lecture-material',
@@ -9,6 +10,7 @@ import { LectureDTO } from '../models/DTO/LectureDTO';
 })
 export class LectureMaterialComponent implements OnInit{
 
+  src : string = `${environment.apiURL}/uploads/`
   lecture: LectureDTO = 
     {
       id: 1,
