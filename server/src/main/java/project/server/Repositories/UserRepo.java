2a2253e91cc89cbdf4d2c5905191d66df76d4fe5
Rami Jebali <jebalirami7@gmail.com>
2024-04-30 19:50:38 +0100

    Add forgotten password feature

--- server/src/main/java/project/server/Repositories/UserRepo.java
+++ server/src/main/java/project/server/Repositories/UserRepo.java
@@ -3,8 +3,10 @@ package project.server.Repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import project.server.Entities.User;
 
@@ -14,9 +16,11 @@ public interface UserRepo extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findUserByEmail(String email);
 
+    Optional<User> findByUsername(String username);
 
-
-	Optional<User> findByUsername(String username);
-
+    @Transactional
+    @Modifying
+    @Query("update User u set u.password = ?2 where u.email = ?1")
+    void updatePassword(String email, String password);
 
 }
--- server/src/main/java/project/server/Services/Implementations/EmailSenderServiceImpl.java
+++ server/src/main/java/project/server/Services/Implementations/EmailSenderServiceImpl.java
@@ -1,8 +1,15 @@
 package project.server.Services.Implementations;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+
+import project.server.Entities.User;
+import project.server.Repositories.UserRepo;
 import project.server.Services.EmailSenderService;
 
 @Service
@@ -10,6 +17,9 @@ public class EmailSenderServiceImpl implements EmailSenderService {
 
     @Autowired
     private JavaMailSender mailSender;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @Override
     public void sendEmail(String to, String subject, String message) {
@@ -19,6 +29,28 @@ public class EmailSenderServiceImpl implements EmailSenderService {
         simpleMailMessage.setText(message);
 
         this.mailSender.send(simpleMailMessage);
+    }
+
+    public String resetPassword(String email) {
+        User user = userRepo.findUserByEmail(email).orElse(null);
+        if (user == null) {
+            return "User not found";
+        }
+        SecureRandom random = new SecureRandom();
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+        StringBuilder newPassword = new StringBuilder();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        for (int i = 0; i < 10; i++) {
+            int index = random.nextInt(characters.length());
+            newPassword.append(characters.charAt(index));
+        }
+
+        String message = "Your new password is: " + newPassword.toString();
+        sendEmail(email, "Reset Your Password", message);
+
+        userRepo.updatePassword(email, encoder.encode(newPassword.toString()));
+        return "Email sent successfully";
     }
 
 }
